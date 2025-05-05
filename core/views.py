124d6f6197b51a0d7aab49460dd98682aa295829
Yuhesh Pandian P J <yuheshpandian@gmail.com>
2025-05-05 20:42:28 +0530

    configured app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Views for this project
+def home(request):
+    return HttpResponse("helllo world")
