f35942d3e53e9588c76b6d334be7e5e9f0e79667
Yuhesh Pandian P J <yuheshpandian@gmail.com>
2025-05-06 00:23:03 +0530

    created footer for the website and also a base.css file for all page to inherit

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from core import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
+    path("about/", views.home, name="about"),
+    path("contact/", views.home, name="contact"),
+    path("testimonials/", views.home, name="testimonials"),
+    path("services/", views.home, name="services"),
 ]
